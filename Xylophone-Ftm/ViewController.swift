3ec6b84a94a16de614a9f3f58e08dd33b17536bb
FatemeFeraghi <feraghi.fateme@gmail.com>
2021-08-04 10:31:14 -0400

    Play sounds when the buttons are pressed

---
+++ Xylophone-Ftm/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Xylophone-Ftm
+//
+//  Created by Fateme on 2021-08-03.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        //Code executes after 0.2 second delay
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+}
